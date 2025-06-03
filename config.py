8d991a99bd4d37cf6b199aa590fbdec9128cb9c8
Noah Guarini <noah@mac.mynetworksettings.com>
2025-06-02 21:56:32 -0400

    Update config.py to connect to PythonAnywhere MySQL database

--- config.py
+++ config.py
@@ -1,12 +1,9 @@
 import os
 
 class Config:
-    # change the string below to anything random & secret before production
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
 
-    # during local dev use SQLite in the project folder
-    SQLALCHEMY_DATABASE_URI = (
-        os.environ.get("DATABASE_URL") or
-        "sqlite:///" + os.path.join(os.path.dirname(__file__), "app.db")
-    )
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or \
+        "mysql+mysqlconnector://devgreeny:ziknip-kAvni2-duhvek@devgreeny.mysql.pythonanywhere-services.com/devgreeny$default"
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
